44ac19f9278995da657ed9eec349ede6d6b5c0f3
dthiermann <72568589+dthiermann@users.noreply.github.com>
2023-09-27 10:45:40 -0700

    Added complete proof of "Square of even is even"

--- some-more-proofs.rkt
+++ some-more-proofs.rkt
@@ -1,15 +1,41 @@
-(implies (even m) (even (square m)))
+; theorem
+(implies (even m)
+         (even (square m)))
 
-(define (even n) (there-exists natural k
-                  (equal (product 2 k) n)))
+; relevant defs
+(define (even m)
+  (there-exists integer n
+                (equal (product 2 n) m)))
 
+; statements in proof
+(even m)
+;assumption
+
+(there-exists integer n
+              (equal (product 2 n) m))
 ; by def of even
-(there-exists natural k
- (equal (product 2 k) m))
 
-(implies (equal a b) (equal (f a) (f b)))
+(equal (product 2 n) m)
+;taking witness from existence statement
+
+(equal (square (product 2 n)) (square m))
+; a = b implies (f a) = (f b)
+; where f = square
+
+(equal (square m) (product 4 (square n)))
+; definition of square and associativity and commutativity of multiplication
+
+(equal (square m) (product 2 (product 2 (square n))))
+; associativity and commutativity of multiplication
+
+(there-exists integer k
+              (equal (square m) (product 2 k)))
+; using the witness (product 2 (square n)) as k to write an existence statement 
+
+(even (square m))
+; using definition of even
+
+(implies (even m) (even (square m)))
+; from derivation beginning with (even m) and ending with (even (square m))
 
 
-(for-all natural (a b)
- (implies (equal (product 2 k) m)
-  (equal (square m) (square (
